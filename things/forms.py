1240b152a5dae5c9775bdbc7c0468be0fb9e3199
Chin Wan <k21016106@kcl.ac.uk>
2022-12-02 16:44:01 +0000

    Minor bugs

--- things/forms.py
+++ things/forms.py
@@ -1,10 +1,13 @@
 """Forms of the project."""
 from django import forms
+from things.models import Thing
 
 # Create your forms here.
-class ThingForm(forms.Form):
-    fields = ["name", "description", "quantity"]
-    widget = {
-        "description": forms.Textarea(),
-        "quantity": forms.NumberInput()
-    }
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ["name", "description", "quantity"]
+        widget = {
+            "description": forms.Textarea(),
+            "quantity": forms.NumberInput()
+        }
