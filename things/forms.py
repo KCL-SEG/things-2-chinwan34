eb9b098f51926f7947c1cc0bc510e43c09e2b01f
Chin Wan <k21016106@kcl.ac.uk>
2022-12-02 16:38:10 +0000

    First try

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
+from django import forms
 
 # Create your forms here.
+class ThingForm(forms.Form):
+    fields = ["name", "description", "quantity"]
+    widget = {
+        "description": forms.Textarea(),
+        "quantity": forms.NumberInput()
+    }
