8f80b949c27b5c336900eebd259d3fb86214ed8e
Chin Wan <k21016106@kcl.ac.uk>
2022-12-02 16:55:47 +0000

    Try possible solution

--- things/forms.py
+++ things/forms.py
@@ -7,7 +7,7 @@ class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ["name", "description", "quantity"]
-        widget = {
+        widgets = {
             'description': forms.Textarea,
             'quantity': forms.NumberInput
         }
